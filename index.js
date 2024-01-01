5c287b9e8ec2a30c03146f21c318aa7d399c64ee
Funminiyi Adekunle <nhiyhe@gmail.com>
2024-01-01 11:18:22 +0000

    add customer endpoint

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ app.get("/", (req, res) => {
   res.send("Welcome to Node Js World");
 });
 
+app.get("/api/customers", (req, res) => {
+  res.send([
+    { id: 1, name: "james" },
+    { id: 2, name: "Abyy" },
+  ]);
+});
+
 app.listen(5000, () => console.log("Server is running on port 5000"));
